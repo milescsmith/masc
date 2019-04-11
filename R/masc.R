4f5b450a732b534bafad7692c05dedd79d389443
Miles Smith <miles-smith@omrf.org>
2019-04-11 15:11:06 -0500

    Replaced paste with glue functions.  Overhauled how reporting is generated - now reports all odds ratios.

--- R/masc.R
+++ R/masc.R
@@ -10,6 +10,12 @@
 #' @param verbose TRUE/FALSE
 #'
 #' @importFrom lme4 glmer glmerControl confint.merMod fixef
+#' @importFrom glue glue glue_collapse
+#' @importFrom dplyr mutate recode
+#' @importFrom tidyr spread pivot_longer
+#' @importFrom stringr str_remove
+#' @importFrom purrr map_dfr map_chr map
+#' @importFrom tibble as_tibble
 #' 
 #' @return data frame containing calculated association p-values and odds ratios for each cluster tested
 #' @export
@@ -37,14 +43,15 @@ MASC <- function(dataset,
                  contrast, 
                  random_effects = NULL, 
                  fixed_effects = NULL,
-                 verbose = FALSE, 
+                 verbose = FALSE,
                  save_models = FALSE, 
                  save_model_dir = NULL) {
+  
   # Check inputs
   if (is.factor(dataset[[contrast]]) == FALSE) {
     stop("Specified contrast term is not coded as a factor in dataset")
   }
-
+  
   # Generate design matrix from cluster assignments
   cluster <- as.character(cluster)
   # Prepend design matrix generated from cluster assignments
@@ -53,99 +60,110 @@ MASC <- function(dataset,
   # Create output list to hold results
   res <- vector(mode = "list", length = length(unique(cluster)))
   names(res) <- colnames(designmat)
-
+  
   # Create model formulas
   if (!is.null(fixed_effects) && !is.null(random_effects)) {
-    model_rhs <- paste0(c(
-      paste0(fixed_effects, collapse = " + "),
-      paste0("(1|", random_effects, ")", collapse = " + ")
-    ),
-    collapse = " + "
-    )
+    model_rhs <- glue_collapse(c(glue_collapse(glue("{fixed_effects}"), 
+                                               sep = " + "),
+                                 glue_collapse(glue("(1|{random_effects})"), 
+                                               sep = " + ")),
+                               sep = " + ")
     if (verbose == TRUE) {
-      message(paste("Using null model:", "cluster ~", model_rhs))
+      message(glue("Using null model: cluster ~ {model_rhs}"))
     }
   } else if (!is.null(fixed_effects) && is.null(random_effects)) {
-    model_rhs <- paste0(fixed_effects, collapse = " + ")
+    model_rhs <- glue_collapse(glue("{fixed_effects}"), 
+                               sep = " + ")
     if (verbose == TRUE) {
-      message(paste("Using null model:", "cluster ~", model_rhs))
+      message(glue("Using null model: cluster ~ {model_rhs}"))
       # For now, do not allow models without mixed effects terms
       stop("No random effects specified")
     }
   } else if (is.null(fixed_effects) && !is.null(random_effects)) {
-    model_rhs <- paste0("(1|", random_effects, ")", collapse = " + ")
+    model_rhs <- glue_collapse(glue("(1|{random_effects})"), 
+                               sep = " + ")
     if (verbose == TRUE) {
-      message(paste("Using null model:", "cluster ~", model_rhs))
+      message(glue("Using null model: cluster ~ {model_rhs}"))
     }
   } else {
     model_rhs <- "1" # only includes intercept
     if (verbose == TRUE) {
-      message(paste("Using null model:", "cluster ~", model_rhs))
+      message(glue("Using null model: cluster ~ {model_rhs}"))
       stop("No random or fixed effects specified")
     }
   }
-
+  
   # Initialize list to store model objects for each cluster
   cluster_models <- vector(
     mode = "list",
     length = length(colnames(designmat))
   )
   names(cluster_models) <- colnames(designmat)
-
+  
   # Run nested mixed-effects models for each cluster
-  for (i in seq_along(colnames(designmat))) {
+  cluster_models <- map(seq_along(colnames(designmat)), function(i){
     test_cluster <- colnames(designmat)[i]
     if (verbose == TRUE) {
-      message(paste("Creating logistic mixed models for", test_cluster))
+      message(glue("Creating logistic mixed models for {test_cluster}"))
     }
-    null_fm <- as.formula(paste0(c(
-      paste0(test_cluster, " ~ 1 + "),
-      model_rhs
-    ), collapse = ""))
-    full_fm <- as.formula(paste0(c(
-      paste0(test_cluster, " ~ ", contrast, " + "),
-      model_rhs
-    ), collapse = ""))
+    null_fm <- as.formula(glue_collapse(c(glue("{test_cluster} ~ 1 + "), 
+                                          model_rhs), 
+                                        sep = ""))
+    full_fm <- as.formula(glue_collapse(c(glue("{test_cluster} ~ {contrast} + "), 
+                                          model_rhs), 
+                                        sep = ""))
     # Run null and full mixed-effects models
     null_model <- glmer(formula = null_fm, 
                         data = dataset,
                         family = binomial, 
                         nAGQ = 1, 
                         verbose = 0,
-                        control = glmerControl(optimizer = "bobyqa")
-                        )
+                        control = glmerControl(optimizer = "bobyqa"))
     full_model <- glmer(formula = full_fm, 
                         data = dataset,
                         family = binomial, 
                         nAGQ = 1, 
                         verbose = 0,
-                        control = glmerControl(optimizer = "bobyqa")
-                        )
+                        control = glmerControl(optimizer = "bobyqa"))
     model_lrt <- anova(null_model, 
                        full_model)
-    # calculate confidence intervals for contrast term beta
-    contrast_lvl2 <- paste0(contrast, 
-                            levels(dataset[[contrast]])[2])
-    contrast_ci <- confint.merMod(full_model,
-                                  method = "Wald",
-                                  parm = contrast_lvl2
-                                  )
+    message("calculate confidence intervals for contrast terms")
+    contrast_lvl2 <- map_chr(2:length(levels(dataset[[contrast]])), function(j){
+      paste0(contrast,
+             levels(dataset[[contrast]])[j])
+    })
+    contrast_ci <- map_dfr(2:length(levels(dataset[[contrast]])), function(j){
+      confint.merMod(full_model,
+                     method = "Wald",
+                     parm = contrast_lvl2[[j-1]]) %>% 
+        as_tibble(rownames = "contrast")
+    })
+    
     # Save model objects to list
     cluster_models[[i]]$null_model <- null_model
     cluster_models[[i]]$full_model <- full_model
     cluster_models[[i]]$model_lrt <- model_lrt
     cluster_models[[i]]$confint <- contrast_ci
+    cluster_models
+  })
+  
+  # Remove the extra null entries
+  for(i in seq(cluster_models)){
+    cluster_models[[i]][sapply(cluster_models[[i]], is.null)] <- NULL
   }
-
+  
   # Organize results into output dataframe
-  output <- data.frame(cluster = attributes(designmat)$dimnames[[2]],
-                       size = colSums(designmat)
-  )
-  output$model.pvalue <- sapply(cluster_models, function(x) x$model_lrt[["Pr(>Chisq)"]][2])
-  output[[paste(contrast_lvl2, "OR", sep = ".")]] <- sapply(cluster_models, function(x) exp(fixef(x$full)[[contrast_lvl2]]))
-  output[[paste(contrast_lvl2, "OR", "95pct.ci.lower", sep = ".")]] <- sapply(cluster_models, function(x) exp(x$confint[contrast_lvl2, "2.5 %"]))
-  output[[paste(contrast_lvl2, "OR", "95pct.ci.upper", sep = ".")]] <- sapply(cluster_models, function(x) exp(x$confint[contrast_lvl2, "97.5 %"]))
-
+  output <- map_dfr(cluster_models, function(i){
+    contrast_name <- names(i)
+    confint <- i[[1]][["confint"]] %>% 
+      pivot_longer(cols = c(`2.5 %`, `97.5 %`)) %>% 
+      mutate(type = str_remove(names(i), "cluster"),
+             model.pval = i[[1]]$model_lrt[["Pr(>Chisq)"]][2],
+             OR = exp(fixef(i[[1]]$full_model))[.$contrast],
+             contrast = str_remove(contrast, "classification"))
+  }) %>% spread(key = "name", value = "value")
+  
+  colnames(output) %<>% recode(`2.5 %` = "lower_ci_95pct", `97.5 %` = "upper_ci_95pct")
   # Return MASC results and save models if specified
   if (save_models == TRUE) {
     saveModelObj(cluster_models, save_dir = save_model_dir)
---
+++ R/utils-pipe.R
@@ -0,0 +1,23 @@
+#' Pipe operator
+#'
+#' See \code{magrittr::\link[magrittr]{\%>\%}} for details.
+#'
+#' @name %>%
+#' @rdname pipe
+#' @keywords internal
+#' @export
+#' @importFrom magrittr %>%
+#' @usage lhs \%>\% rhs
+NULL
+
+#' Compound assignment pipe operator
+#'
+#' See \code{magrittr::\link[magrittr]{\%<>\%}} for details.
+#'
+#' @name %<>%
+#' @rdname compound
+#' @keywords internal
+#' @export
+#' @importFrom magrittr %<>%
+#' @usage lhs \%<>\% rhs
+NULL
